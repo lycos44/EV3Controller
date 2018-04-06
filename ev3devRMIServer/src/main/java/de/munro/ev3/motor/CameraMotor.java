90a6ae51ec69fd312a950a8c5a38fe3c29bd1064
lycos44 <wolfgang.auer@gmx.net>
2018-04-06 06:03:50 +0200

    added junit tests

--- ev3devRMIServer/src/main/java/de/munro/ev3/motor/CameraMotor.java
+++ ev3devRMIServer/src/main/java/de/munro/ev3/motor/CameraMotor.java
@@ -2,14 +2,19 @@ package de.munro.ev3.motor;
 
 import de.munro.ev3.rmi.EV3devConstants;
 import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import lejos.utility.Delay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CameraMotor extends Motor{
+public class CameraMotor extends Motor {
     private static final Logger LOG = LoggerFactory.getLogger(CameraMotor.class);
 
     private static CameraMotor instance;
     private EV3LargeRegulatedMotor motor;
+
+    private final int homePosition = 0;
+    private int leftmostPosition = 0;
+    private int rightmostPosition = 0;
 
     public static CameraMotor getInstance() {
         if (null == instance) {
@@ -19,9 +24,9 @@ public class CameraMotor extends Motor{
     }
 
     private CameraMotor() {
-        this.motor = createMotor(EV3devConstants.CAMERA_MOTOR_PORT);
+        this.motor = createMotor(EV3devConstants.CAMERA_MOTOR_PORT, Polarity.INVERSED);
         if (null == this.motor) {
-            this.motor = createMotor(EV3devConstants.CAMERA_MOTOR_PORT);
+            this.motor = createMotor(EV3devConstants.CAMERA_MOTOR_PORT, Polarity.INVERSED);
         }
     }
 
@@ -33,5 +38,33 @@ public class CameraMotor extends Motor{
     @Override
     public EV3LargeRegulatedMotor getMotor() {
         return motor;
+    }
+
+    @Override
+    public void init() {
+        LOG.debug("init()");
+        backwardTillStalled();
+        getMotor().resetTachoCount();
+        int left = getMotor().getTachoCount();
+        LOG.debug("left: {}", left);
+        forwardTillStalled();
+        int right = getMotor().getTachoCount();
+        LOG.debug("right: {}", right);
+        int home = (left+right)/2;
+        leftmostPosition = home;
+        rightmostPosition = -home;
+        getMotor().rotateTo(home);
+        Delay.msDelay(1000);
+
+        getMotor().resetTachoCount();
+        LOG.debug("(left, home, right): ({}, {}, {})", leftmostPosition, homePosition, rightmostPosition);
+    }
+
+    public int getLeftmostPosition() {
+        return leftmostPosition;
+    }
+
+    public int getRightmostPosition() {
+        return  rightmostPosition;
     }
 }
